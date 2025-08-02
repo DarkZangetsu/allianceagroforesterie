bd2d3f12d918f4f2890cf80552211b4c216bbbbe
DarkZangetsu <113225391+DarkZangetsu@users.noreply.github.com>
2025-08-02 11:14:49 +0300

    build next

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,16 +1,21 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: 'export',
+//   trailingSlash: true,
+//   images: {
+//     unoptimized: true
+//   },
+//   eslint: {
+//     ignoreDuringBuilds: false,
+//   },
+//   typescript: {
+//     ignoreBuildErrors: false,
+//   },
+// }
+
+// export default nextConfig
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  images: {
-    unoptimized: true
-  },
-  eslint: {
-    ignoreDuringBuilds: false,
-  },
-  typescript: {
-    ignoreBuildErrors: false,
-  },
 }
 
 export default nextConfig
